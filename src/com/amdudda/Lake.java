fd34730c6f160598e25465f9f58849aaf7b3a9af
amdudda <tk0654wm@go.minneapolis.edu>
2015-10-03 21:30:54 -0500

    Added attributes for Lake & get/set methods for some of these.

--- src/com/amdudda/Lake.java
+++ src/com/amdudda/Lake.java
@@ -1,7 +1,29 @@
 package com.amdudda;
 
+import java.util.ArrayList;
+
 /**
  * Created by amdudda on 10/3/15.
  */
 public class Lake {
+    // stores the times taken to run around various lakes
+
+    // attributes of Lake object
+    private String name;
+    private ArrayList<Double> times;
+
+    // TODO: Decide whether constructor is needed for this implementation
+
+    // getter for name and times and setter for name
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public ArrayList<Double> getTimes() {
+        return times;
+    }
 }
